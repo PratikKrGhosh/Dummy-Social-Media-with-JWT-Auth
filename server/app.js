403f00d0800f4e2699ec11c467f766fdab985a36
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-17 16:32:33 +0530

    added documentation by commenting in app.js file

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// importing all modules and files
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
@@ -5,8 +6,10 @@ import authRouter from "./routes/auth.router.js";
 import db from "./config/db.js";
 import authMiddleware from "./middlewares/auth.middleware.js";
 
+// creating app
 const app = express();
 
+// setting all middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -16,18 +19,22 @@ app.use(
   express.static(path.join(import.meta.dirname, "..", "client", "public"))
 );
 
+// connecting to the Database
 db();
 
+// creating all middlewares
 app.use(authMiddleware);
 app.use((req, res, next) => {
   res.locals.user = req.user;
   return next();
 });
 
+// creating all Routes
 app.use("/", authRouter);
 
 app.get("/", (req, res) => {
   res.status(200).render("index");
 });
 
+// exporting app
 export default app;
